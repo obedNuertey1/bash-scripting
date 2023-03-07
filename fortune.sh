fc624fa518da46186b62d00c5d9036e545ba9063
Obed Nuertey <onuertey1997@gmail.com>
2023-03-07 16:02:14 +0000

    first commit

---
+++ fortune.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Program to tell a persons fortune
+echo -e "\n~~ Fortune Teller ~~\n"
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM%6 ))
+echo  -e "\n${RESPONSES[$N]}"
+GET_FORTUNE(){
+	if [[ ! $1 ]]
+	then
+		echo Ask a yes or no question:
+		read QUESTION
+	else
+		echo Try again. Make sure it ends with a question mark:
+	fi
+}
+
+GET_FORTUNE
+
+until [[ $QUESTION =~ \?$ ]]
+do
+	GET_FORTUNE again
+done
+	
